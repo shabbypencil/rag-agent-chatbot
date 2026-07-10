9cebddfecab2c6658ef0d2e963dac9d8c7d3a63a
shabbypencil <tohhaoyi.amos@gmail.com>
2026-07-11 01:07:38 +0800

    Ensure chroma directory is auto created if it doesnt exist

--- backend/db/chroma_client.py
+++ backend/db/chroma_client.py
@@ -1,9 +1,10 @@
 import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from backend.core.config import CHROMA_DIR, EMBEDDING_MODEL
+from pathlib import Path
 
 embedding_function = SentenceTransformerEmbeddingFunction(model_name=EMBEDDING_MODEL)
-
+Path(CHROMA_DIR).mkdir(parents=True, exist_ok=True)
 client = chromadb.PersistentClient(path=str(CHROMA_DIR))
 
 def get_or_create_collection(name: str):
